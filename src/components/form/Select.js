0644c8ff86076574608402af45d77c9182a06c70
Nicole-Marinho-Espindola <nicole.m.espindola@gmail.com>
2024-05-27 20:11:07 -0300

    fixes

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,12 +1,16 @@
 import styles from "./css/select.module.css";
+import categories from "../../mocks/db_projects.json"
 
 function Select({ text, name, options, handleOnChange }) {
+  
   return (
     <div className={styles.form_control}>
       <div className={styles.label_container}>
         <label htmlFor={name}>{text}</label>
         <select name={name} id={name} onChange={handleOnChange}>
-          <option>Choose an option</option>
+          <options>
+            <option>{categories.name}</option>
+          </options>
         </select>
       </div>
     </div>
